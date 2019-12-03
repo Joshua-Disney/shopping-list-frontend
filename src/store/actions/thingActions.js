80e8235acd3fe305c19538f977d448a4f26fba4d
Mr. Disney <joshua.a.disney@gmail.com>
2019-12-02 21:33:36 -0700

    added await to thingActions

--- src/store/actions/thingActions.js
+++ src/store/actions/thingActions.js
@@ -7,7 +7,7 @@ export const FETCH_THING_FAILURE = "FETCH_THING_FAILURE";
 export const getThing = () => async dispatch => {
   dispatch({ type: FETCH_THING_START });
   try {
-    const result = axios.get("https://website.com");
+    const result = await axios.get("https://website.com");
     dispatch({ type: FETCH_THING_SUCCESS, payload: result.data });
   } catch (error) {
     dispatch({ type: FETCH_THING_FAILURE, payload: error });
