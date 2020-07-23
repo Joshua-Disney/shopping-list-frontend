10df53abfd499d14b1e23fc120a7639cd6640275
Mr. Disney <joshua.a.disney@gmail.com>
2020-07-22 21:53:24 -0700

    Create input templates for email and password updating

--- src/components/Settings/settings.js
+++ src/components/Settings/settings.js
@@ -1,51 +1,106 @@
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
+const initialState = {
+  currentValue: "",
+  updatedValue: "",
+  updatingEmail: false,
+  updatingPassword: false,
+  deletingAccount: false,
+};
+
 const Settings = (props) => {
   const [state, setState] = useState({
-    updatingEmail: false,
-    updatingPassword: false,
-    deletingAccount: false,
+    ...initialState,
   });
 
-  useEffect(() => {
-    console.log("This is the settings page.");
-  }, []);
-
-  useEffect(() => {
-    console.log("changed state", state);
-  }, [state]);
-
   const updateState = (event, action) => {
-    console.log("action", action);
     event.preventDefault();
-    setState({ ...state, [action]: !state[action] });
+    setState({ ...initialState, [action]: !state[action] });
   };
 
   return (
     <div>
-      <h2>This is the settings page! :D</h2>
-      <NavLink className="nav-item" to="/">
+      <h2>User settings</h2>
+      <NavLink className="give this a classname later that's important" to="/">
         Return Home
       </NavLink>
-      <p onClick={(e) => updateState(e, "updatingEmail")}>
+      <p onClick={(event) => updateState(event, "updatingEmail")}>
         Update email address
       </p>
       {state.updatingEmail && (
-        <form>
-          <h3>this will be a form</h3>
+        <form
+          className="settings-form"
+          onSubmit={(event) => {
+            event.preventDefault();
+            console.log(
+              "current value: ",
+              state.currentValue,
+              "updated value: ",
+              state.updatedValue
+            );
+          }}
+        >
+          <input
+            className="settings-input"
+            type="text"
+            value={state.currentValue}
+            placeholder="Current email address"
+            onChange={(event) =>
+              setState({ ...state, currentValue: event.target.value })
+            }
+          />
+          <input
+            className="settings-input"
+            type="text"
+            value={state.updatedValue}
+            placeholder="New email address"
+            onChange={(event) =>
+              setState({ ...state, updatedValue: event.target.value })
+            }
+          />
+          <button className="settings-button">Submit</button>
         </form>
       )}
       <p onClick={(e) => updateState(e, "updatingPassword")}>Reset password</p>
       {state.updatingPassword && (
-        <form>
-          <h3>this will be a form</h3>
+        <form
+          className="settings-form"
+          onSubmit={(event) => {
+            event.preventDefault();
+            console.log(
+              "current value: ",
+              state.currentValue,
+              "updated value: ",
+              state.updatedValue
+            );
+          }}
+        >
+          <input
+            className="settings-input"
+            type="text"
+            value={state.currentValue}
+            placeholder="New password"
+            onChange={(event) =>
+              setState({ ...state, currentValue: event.target.value })
+            }
+          />
+          <input
+            className="settings-input"
+            type="text"
+            value={state.updatedValue}
+            placeholder="Confirm password"
+            onChange={(event) =>
+              setState({ ...state, updatedValue: event.target.value })
+            }
+          />
+          <button className="settings-button">Submit</button>
         </form>
       )}
       <p onClick={(e) => updateState(e, "deletingAccount")}>Delete account</p>
       {state.deletingAccount && (
         <form>
-          <h3>this will be a form</h3>
+          <h3>this will be a model to confirm deletion</h3>
         </form>
       )}
     </div>
