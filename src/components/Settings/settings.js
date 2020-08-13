85a62eb5d1a2a37129c5b08255119e90e051b990
Mr. Disney <joshua.a.disney@gmail.com>
2020-08-13 11:02:17 -0700

    Fix update email rendering bug

--- src/components/Settings/settings.js
+++ src/components/Settings/settings.js
@@ -45,7 +45,7 @@ const Settings = (props) => {
 
   useEffect(() => {
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (state.updatedValue > 1) {
+    if (state.updatedValue.length > 1) {
       setState({ ...state, isEmail: !!state.updatedValue.match(emailRegex) });
     }
     console.log(
