d36c46bcfd19229dc40d769b6520790d92617c8e
Mr. Disney <joshua.a.disney@gmail.com>
2020-08-18 17:41:09 -0700

    Create delete account action

--- src/store/actions/accountActions.js
+++ src/store/actions/accountActions.js
@@ -6,6 +6,9 @@ export const GET_ACCOUNT_FAILURE = "GET_ACCOUNT_FAILURE";
 export const UPDATE_ACCOUNT_START = "UPDATE_ACCOUNT_START";
 export const UPDATE_ACCOUNT_SUCCESS = "UPDATE_ACCOUNT_SUCCESS";
 export const UPDATE_ACCOUNT_FAILURE = "UPDATE_ACCOUNT_FAILURE";
+export const DELETE_ACCOUNT_START = "DELETE_ACCOUNT_START";
+export const DELETE_ACCOUNT_SUCCESS = "DELETE_ACCOUNT_SUCCESS";
+export const DELETE_ACCOUNT_FAILURE = "DELETE_ACCOUNT_FAILURE";
 
 export const getAccount = (id) => async (dispatch) => {
   dispatch({ type: GET_ACCOUNT_START });
@@ -29,5 +32,19 @@ export const updateAccount = (id, account) => async (dispatch) => {
     dispatch({ type: UPDATE_ACCOUNT_SUCCESS, payload: result.data });
   } catch (error) {
     dispatch({ type: UPDATE_ACCOUNT_FAILURE, payload: error });
+  }
+};
+
+export const deleteAccount = (id, password) => async (dispatch) => {
+  dispatch({ type: DELETE_ACCOUNT_START });
+  try {
+    const result = await axiosWithAuth().delete(
+      `https://disneys-shopping-list-backend.herokuapp.com/api/accounts/${id}`,
+      password
+    );
+    dispatch({ type: DELETE_ACCOUNT_SUCCESS, payload: result.data });
+  } catch (error) {
+    console.log(error);
+    dispatch({ type: DELETE_ACCOUNT_FAILURE, payload: error });
   }
 };
