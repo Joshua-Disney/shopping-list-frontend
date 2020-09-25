2f5db4c146380888afcddd8276e02efe185b4e69
Thomas Dillard <thomas.dillard.studios@gmail.com>
2020-09-24 19:12:26 -0700

    Fixed Navbar layout

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { NavLink } from "react-router-dom";
 
 import { getThing, logout, getAccount } from "../../store/actions";
-import Hamburger from "../../ui/Hamburger";
 
 import Profile from "../Profiles/profile";
 
 const Home = (props) => {
-  const [displayMenu, setDisplayMenu] = useState(true);
   useEffect(() => {
     let account_id = props.account_id;
     if (!account_id) {
@@ -23,32 +20,6 @@ const Home = (props) => {
     <section className="app-main">
       {props.account.id > 0 ? (
         <>
-          <div className="red-green-refactor specific">
-            <nav className="bg-green-800">
-              <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative">
-                <Hamburger display={displayMenu} onClick={() => setDisplayMenu(!displayMenu)} />
-                <div
-                  className={`toggle-menu ${displayMenu ? "hidden" : "block"} absolute right-0 mt-2 py-2 w-48 bg-white rounded-lg shadow-xl`}
-                >
-                  <NavLink className="block px-4 py-2 text-gray-800 hover:bg-blue-200" to="/add-profile">
-                    Add Profile
-                  </NavLink>
-                  <NavLink className="block px-4 py-2 text-gray-800 hover:bg-blue-200" to="/settings">
-                    Settings
-                  </NavLink>
-                  {/* this should be a link */}
-                  <button
-                    className="w-block px-4 py-2 text-gray-800 hover:bg-blue-200 text-left w-full"
-                    onClick={(event) => {
-                      props.logout(props);
-                    }}
-                  >
-                    Log Out
-                  </button>
-                </div>
-              </div>
-            </nav>
-          </div>
           <div className="red-green-refactor">
             {/* <Account account={props.account} /> */}
             {props.account.profiles.map((profile) => {
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import Hamburger from '../../ui/Hamburger';
+
+const Navbar = (props) => {
+	const [displayMenu, setDisplayMenu] = useState(true);
+  return (
+    <nav className="bg-green-800">
+      <div className="px-6 sm:px-8 lg:px-10 container mx-auto relative">
+        <Hamburger
+          display={displayMenu}
+          onClick={() => setDisplayMenu(!displayMenu)}
+        />
+        <div
+          className={`toggle-menu ${
+            displayMenu ? "hidden" : "block"
+          } absolute right-0 mt-2 py-2 w-48 bg-white rounded-lg shadow-xl`}
+        >
+          <NavLink
+            className="block px-4 py-2 text-gray-800 hover:bg-blue-200"
+            to="/add-profile"
+          >
+            Add Profile
+          </NavLink>
+          <NavLink
+            className="block px-4 py-2 text-gray-800 hover:bg-blue-200"
+            to="/settings"
+          >
+            Settings
+          </NavLink>
+          {/* this should be a link */}
+          <button
+            className="w-block px-4 py-2 text-gray-800 hover:bg-blue-200 text-left w-full"
+            onClick={(event) => {
+              props.logout(props);
+            }}
+          >
+            Log Out
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
