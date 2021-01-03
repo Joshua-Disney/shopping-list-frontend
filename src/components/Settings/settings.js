6b62815dd875471550102ad5c59614f05e824b2f
Mr. Disney <joshua.a.disney@gmail.com>
2021-01-03 15:37:44 -0700

    Comment to push branch

--- src/components/Settings/settings.js
+++ src/components/Settings/settings.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
 
+// Something to push
+
 import {
   createUser,
   deleteUser,
