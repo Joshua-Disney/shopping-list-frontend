572655b578902ddd9021da0d071a1c777451c9b2
Thomas Dillard <Thomas.Dillard.studios@gmail.com>
2021-03-02 21:46:42 -0700

    Updated changes for footer

--- src/components/contactFooter.js
+++ src/components/contactFooter.js
@@ -2,12 +2,15 @@ import React from "react";
 
 const ContactFooter = () => {
   return (
-    <div>
-      <p>
-        Thank you for using Dizlist! Have any questions, comments, or derogatory
-        remarks? Please email me as contact@joshuadisney.com
-      </p>
-    </div>
+    <footer class="bg-green-50">
+      <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+        <div class="mt-8 md:mt-0 md:order-1">
+          <p class="text-center text-base text-gray-400">
+            &copy; 2020 All rights reserved. Thank you for using Dizlist! Have any questions, comments, or derogatory remarks? Please email me as <address>contact@joshuadisney.com</address>
+          </p>
+        </div>
+      </div>
+    </footer>
   );
 };
 
