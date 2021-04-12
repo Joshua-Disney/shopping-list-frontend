df101a1f3ef595c4dbe364d26284c6d03409c733
Mr. Disney <joshua.a.disney@gmail.com>
2021-04-11 19:59:38 -0700

    Started turnary expression in listEditor

--- src/components/listEditor.jsx
+++ src/components/listEditor.jsx
@@ -1,27 +1,43 @@
-import React from "react"
+import React, { useState } from "react"
 import { connect } from "react-redux"
 
-import { editPofile, deleteProfile } from "../store/actions"
+import { editProfile, deleteProfile } from "../store/actions"
 import { H3 } from "../ui/Titles"
 
 const ListEditor = (props) => {
 
-    const state = {
+    const [state, setState] = useState({
         isEditing: false,
         isDeleting: false,
+    })
+
+    const [value, setValue] = useState(props.name || "")
+
+    const changeToTrue = (event) => {
+        setState({ ...state, [event.target.name] : true })
     }
+
     return (
         <div className="flex???">
-            <H3>{props.name}</H3>
-            <button>Pencil button</button>
-            <section className="hidden-p-tag">
-                <p>Edit</p>
-                <p>Delete</p>
-            </section>
+            {state.isEditing ? 
+                <form>
+                    <input onChange={(event) => setValue(event.target.value)} value={value} placeholder={props.name} />
+                    <button>Confirm</button>
+                    <button>Cancel</button>
+                </form>
+            : <div>
+                <H3>{props.name}</H3>
+                <button>Pencil button</button>
+                <section className="hidden-p-tag">
+                    <button onClick={changeToTrue} name="isEditing">Edit</button>
+                    <button name="isDeleting">Delete</button>
+                </section>
+            </div>
+            }
         </div>
     )
 }
 
-const mapDispatchToProps = { editPofile, deleteProfile }
+const mapDispatchToProps = { editProfile, deleteProfile }
 
 export default connect(null, mapDispatchToProps)(ListEditor);
